4cb5a2b62ff9f074c7daa9bfa40302db556e376f
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-05-05 22:40:04 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.write('oi')
 st.image('https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80')
 
-st.text_input('Busca por nome', placeholder='Digite um nome de Pokémon')
+nome = st.text_input('Busca por nome', placeholder='Digite um nome de Pokémon')
